a5165933f6ae4634567cd5707ac567d10f406554
Kyle Williquette <kyle.c.williquette@gmail.com>
2015-08-16 09:26:52 -0400

    Updated to new METHOD support

--- example/gwitch_example.go
+++ example/gwitch_example.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"github.com/Vaultpls/Gwitch"
 )
 
 func main() {
-	t := gwitch.New("yourbotname", "your oauth here", "#twitchstream", nil)
+	t := gwitch.New("quanticbot", "your own oauth", "#vaultpls", nil)
 
 	err := t.Connect()
 
@@ -15,10 +14,29 @@ func main() {
 	}
 
 	for {
-		user, message := t.ReadMessage()
-		if message == "!hi" {
-			t.SendMessage("Hi " + user + "!")
+
+		data := t.ReadData()
+
+		if data.Method == "MESSAGE" && data.Message == "!hi" {
+
+			t.SendMessage("Hi " + data.Username + "!  I am " + t.Username + "!")
+
 		}
-		fmt.Println(user + ": " + message)
+
+		switch {
+
+		case data.Method == "JOIN":
+			t.SendMessage("Hey there " + data.Username + "!")
+
+		case data.Method == "PART":
+			t.SendMessage("Bye " + data.Username + "!")
+
+		case data.Method == "GAINOP":
+			t.SendMessage(data.Username + " gained op!")
+
+		case data.Method == "LOSEOP":
+			t.SendMessage(data.Username + " lost op!")
+
+		}
 	}
 }
